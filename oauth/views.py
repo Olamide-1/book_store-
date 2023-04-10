5f8c718cedd39ea019027b99bdfd61839f6f63df
toluhunter <toluhunter19@gmail.com>
2023-04-10 18:55:32 +0100

    Fixed status code comparison

--- oauth/views.py
+++ oauth/views.py
@@ -74,8 +74,8 @@ class WalletAuthView(generics.GenericAPIView):
             }
         )
 
-        if response != 200:
-            return Response({"status": "failed", "message": f"Unable to Perform Transaction {response.content}"}, status=status.HTTP_400_BAD_REQUEST)
+        if response.status_code != 200:
+            return Response({"status": "failed", "message": f"Unable to Perform Transaction"}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({"status": "status", "message": "Book Purchased"})
 
